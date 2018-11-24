db95aec2664440d7e80fb50a370203320ef6a880
Yaroglek <king_yaroglek@163.com>
2018-11-25 00:21:55 +0800

    update

--- code/Group35/app/src/main/java/com/example/administrator/midtermprojectgruop35/MainActivity.java
+++ code/Group35/app/src/main/java/com/example/administrator/midtermprojectgruop35/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.administrator.midtermprojectgruop35;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -15,6 +18,9 @@ import android.widget.RelativeLayout;
 import java.util.ArrayList;
 import java.util.List;
 
+import jxl.Sheet;
+import jxl.Workbook;
+
 public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout mHead;//标题头
@@ -23,12 +29,19 @@ public class MainActivity extends AppCompatActivity {
     private ListViewAdapter mAdapter;
     private int leftPos;//用于记录CustomHScrollView的初始位置
     private int topPos;
+    private List<Hero> heroList;
+    private Database database;
     CustomHScrollView mScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = new Database(this);
+        heroList = database.listHero();
+        if (heroList.size() == 0) {
+            initHero();
+        }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         initView();
@@ -118,4 +131,50 @@ public class MainActivity extends AppCompatActivity {
         this.leftPos = l;
         this.topPos = t;
     }
+
+    private void initHero() {
+        AssetManager assetManager = getAssets();
+        try {
+            Workbook workbook = Workbook.getWorkbook(assetManager.open("hero_list.xlsx"));
+            Sheet sheet = workbook.getSheet(0);
+            int sheetNum = workbook.getNumberOfSheets();
+            int sheetRows = sheet.getRows();
+            int sheetColumns = sheet.getColumns();
+            for (int i = 1; i < sheetRows; i++) {
+                int id = Integer.valueOf(sheet.getCell(0, i).getContents());
+                String name = sheet.getCell(1, i).getContents();
+                Bitmap icon = ((BitmapDrawable) getResources().getDrawable(getResources().getIdentifier(sheet.getCell(24, i).getContents() + "_icon.png", "mipmap", getBaseContext().getPackageName()))).getBitmap();
+                Bitmap minimapIcon = ((BitmapDrawable) getResources().getDrawable(getResources().getIdentifier(sheet.getCell(24, i).getContents() + "_minimap_icon.png", "mipmap", getBaseContext().getPackageName()))).getBitmap();
+                String chineseName = sheet.getCell(2, i).getContents();
+                String nickname = sheet.getCell(3, i).getContents();
+                Hero.Species species = Hero.Species.values()[Integer.valueOf(sheet.getCell(4, i).getContents())];
+                Hero.AttackMode attackMode = Hero.AttackMode.values()[Integer.valueOf(sheet.getCell(5, i).getContents())];
+                int difficult = Integer.valueOf(sheet.getCell(6, i).getContents());
+                int carry = Integer.valueOf(sheet.getCell(7, i).getContents());
+                int support = Integer.valueOf(sheet.getCell(8, i).getContents());
+                int nuker = Integer.valueOf(sheet.getCell(9, i).getContents());
+                int disabler = Integer.valueOf(sheet.getCell(10, i).getContents());
+                int jungler = Integer.valueOf(sheet.getCell(11, i).getContents());
+                int durable = Integer.valueOf(sheet.getCell(12, i).getContents());
+                int escape = Integer.valueOf(sheet.getCell(13, i).getContents());
+                int pusher = Integer.valueOf(sheet.getCell(14, i).getContents());
+                int initiator = Integer.valueOf(sheet.getCell(15, i).getContents());
+                int strength = Integer.valueOf(sheet.getCell(16, i).getContents());
+                int agility = Integer.valueOf(sheet.getCell(17, i).getContents());
+                int intelligence = Integer.valueOf(sheet.getCell(18, i).getContents());
+                double strengthUp = Double.valueOf(sheet.getCell(19, i).getContents());
+                double agilityUp = Double.valueOf(sheet.getCell(20, i).getContents());
+                double intelligenceUp = Double.valueOf(sheet.getCell(21, i).getContents());
+                int health = Integer.valueOf(sheet.getCell(22, i).getContents());
+                int mana = Integer.valueOf(sheet.getCell(23, i).getContents());
+                database.insertHero(new Hero(id, name, icon, minimapIcon, chineseName, nickname, species, attackMode, difficult, carry, support, nuker, disabler, jungler, durable, escape, pusher, initiator, strength, agility, intelligence, strengthUp, agilityUp, intelligenceUp, health, mana));
+            }
+            workbook.close();
+            heroList = database.listHero();
+        } catch (Exception e) {
+
+        }
+
+    }
+
 }
