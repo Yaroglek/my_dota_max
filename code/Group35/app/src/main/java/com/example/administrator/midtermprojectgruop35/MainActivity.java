3ecc6e9a9ce215aa54607772d3991ae93c04507b
Zhao <2504197515@qq.com>
2018-11-25 20:58:31 +0800

    update

--- code/Group35/app/src/main/java/com/example/administrator/midtermprojectgruop35/MainActivity.java
+++ code/Group35/app/src/main/java/com/example/administrator/midtermprojectgruop35/MainActivity.java
@@ -85,9 +85,11 @@ public class MainActivity extends AppCompatActivity {
         instance = this;
         database = new Database(this);
         heroList = database.listHero();
+        /*
         if (heroList.size() == 0) {
             initHero();
         }
+        */
         collectList = database.listCollect();
         collectFlag = false;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -534,21 +536,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            mScrollView.smoothScrollTo(leftPos, topPos);//每次刷新数据都让CustomHScrollView回到初始位置，避免错乱
-            if (mAdapter != null){
-                mAdapter.notifyDataSetChanged();
-            }
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * 记录CustomHScrollView的初始位置
      * @param l
@@ -559,54 +546,12 @@ public class MainActivity extends AppCompatActivity {
         this.topPos = t;
     }
 
-    private void initHero() {
-        AssetManager assetManager = getAssets();
-        try {
-            Workbook workbook = Workbook.getWorkbook(assetManager.open("hero_list.xls"));
-            Sheet sheet = workbook.getSheet(0);
-            int sheetNum = workbook.getNumberOfSheets();
-            int sheetRows = sheet.getRows();
-            int sheetColumns = sheet.getColumns();
-            for (int i = 1; i < sheetRows; i++) {
-                int id = Integer.valueOf(sheet.getCell(0, i).getContents());
-                String name = sheet.getCell(1, i).getContents();
-                Bitmap icon = ((BitmapDrawable) getResources().getDrawable(getResources().getIdentifier(sheet.getCell(24, i).getContents() + "_icon", "mipmap", getBaseContext().getPackageName()))).getBitmap();
-                Bitmap minimapIcon = ((BitmapDrawable) getResources().getDrawable(getResources().getIdentifier(sheet.getCell(24, i).getContents() + "_minimap_icon", "mipmap", getBaseContext().getPackageName()))).getBitmap();
-                String chineseName = sheet.getCell(2, i).getContents();
-                String nickname = sheet.getCell(3, i).getContents();
-                Hero.Species species = Hero.Species.valueOf(sheet.getCell(4, i).getContents());
-                Hero.AttackMode attackMode = Hero.AttackMode.valueOf(sheet.getCell(5, i).getContents());
-                int difficult = Integer.valueOf(sheet.getCell(6, i).getContents());
-                int carry = Integer.valueOf(sheet.getCell(7, i).getContents());
-                int support = Integer.valueOf(sheet.getCell(8, i).getContents());
-                int nuker = Integer.valueOf(sheet.getCell(9, i).getContents());
-                int disabler = Integer.valueOf(sheet.getCell(10, i).getContents());
-                int jungler = Integer.valueOf(sheet.getCell(11, i).getContents());
-                int durable = Integer.valueOf(sheet.getCell(12, i).getContents());
-                int escape = Integer.valueOf(sheet.getCell(13, i).getContents());
-                int pusher = Integer.valueOf(sheet.getCell(14, i).getContents());
-                int initiator = Integer.valueOf(sheet.getCell(15, i).getContents());
-                int strength = Integer.valueOf(sheet.getCell(16, i).getContents());
-                int agility = Integer.valueOf(sheet.getCell(17, i).getContents());
-                int intelligence = Integer.valueOf(sheet.getCell(18, i).getContents());
-                double strengthUp = Double.valueOf(sheet.getCell(19, i).getContents());
-                double agilityUp = Double.valueOf(sheet.getCell(20, i).getContents());
-                double intelligenceUp = Double.valueOf(sheet.getCell(21, i).getContents());
-                int health = Integer.valueOf(sheet.getCell(22, i).getContents());
-                int mana = Integer.valueOf(sheet.getCell(23, i).getContents());
-                database.insertHero(new Hero(id, name, icon, minimapIcon, chineseName, nickname, species, attackMode, difficult, carry, support, nuker, disabler, jungler, durable, escape, pusher, initiator, strength, agility, intelligence, strengthUp, agilityUp, intelligenceUp, health, mana));
-            }
-            workbook.close();
-            heroList = database.listHero();
-        } catch (Exception e) {
 
-        }
-
-    }
 
     public void updateList() {
         query();
     }
+
 }
 
 
